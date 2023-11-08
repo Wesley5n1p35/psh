1fa86883d4ea73f4ddd3beae21deb7033741b9ff
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-08 01:44:55 -0600

    Update pp.ps1

--- pp.ps1
+++ pp.ps1
@@ -1,4 +1,49 @@
-# Define the Upload-Discord function
+# Define a list of partial titles you want to monitor
+$targetTitles = @("TikTok", "YouTube", "Facebook")
+
+# Define a list of popular browser process names
+$browserProcesses = @("chrome", "firefox", "msedge", "opera")
+
+# Define the interval to check (in seconds)
+$checkInterval = 15
+
+# Outer loop to run continuously
+while ($true) {
+    # Loop until any condition is met
+    $conditionMet = $false
+
+    while (-not $conditionMet) {
+        foreach ($browserName in $browserProcesses) {
+            $currentBrowserProcesses = Get-Process -name $browserName -ErrorAction SilentlyContinue
+            if ($currentBrowserProcesses) {
+                foreach ($process in $currentBrowserProcesses) {
+                    try {
+                        $processTitle = $process.MainWindowTitle
+                        Write-Host "$browserName - Window Title: $processTitle"
+                        if ($targetTitles | ForEach-Object { $processTitle -like "*$_*" }) {
+                            Write-Host "Detected one of the specified titles in $browserName. Closing..."
+                            Stop-Process -Id $process.Id -Force
+                            $conditionMet = $true  # Set the flag to true when any condition is met
+                            break 2  # Exit both inner and outer loops when any condition is met
+                        }
+                    } catch {
+                        # Handle any exceptions that might occur (e.g., due to missing access permissions)
+                    }
+                }
+            }
+        }
+
+        if ($conditionMet) {
+            break  # Exit the outer loop when any condition is met
+        }
+
+        # Sleep for the specified interval (in seconds) before checking again
+        Start-Sleep -Seconds $checkInterval
+    }
+}
+
+# Continue with the rest of your script here
+Write-Host "A condition is met. Continue with the rest of your code here."
 
 
 function Upload-Discord {
